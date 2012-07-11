ae75aa16ccac4b0d172365e4cc3f32ac6269660c
Joshua Rowley <joshua.rowley@nyu.edu>
2012-07-10 20:41:02 -0400

    Finish authentication

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,7 +10,7 @@
 #
 
 class User < ActiveRecord::Base
-  attr_accessible :email, :name
+  attr_accessible :email, :name, :password, :password_confirmation
 
   before_save { |user| user.email = email.downcase }
 
@@ -18,4 +18,7 @@ class User < ActiveRecord::Base
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, format: { with: VALID_EMAIL_REGEX },
   					uniqueness: 	{ case_sensitive: false }
+  validates :password, presence: true, length: { minimum: 6 }
+  has_secure_password
+  validates :password_confirmation, presence: true
 end
