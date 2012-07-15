baf58dca073ea8fccaade04999315083983ace95
Joshua Rowley <joshua.rowley@nyu.edu>
2012-07-15 01:32:10 -0400

    Finish signup/signin/signout

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -20,8 +20,8 @@ describe "Authentication" do
 		describe "with valid information" do
 			let(:user) { FactoryGirl.create(:user) }
 			before do
-				fill_in "Email", with: user.Email
-				fill_in "Password", with: user.Password
+				fill_in "Email", with: user.email
+				fill_in "Password", with: user.password
 				click_button "Sign in"
 			end
 
@@ -29,6 +29,11 @@ describe "Authentication" do
 			it { should have_link('Profile', href: user_path(user)) }
 			it { should have_link('Sign out', href: signout_path) }
 			it { should_not have_link('Sign in', href: signin_path) }
+
+			describe "followed by signout" do
+				before { click_link "Sign out" }
+				it { should have_link('Sign in') }
+			end
 		end
 	end
 end
