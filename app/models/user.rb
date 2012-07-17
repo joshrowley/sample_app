edeb1d805313083cff1f55bc55e808b0139abf79
Joshua Rowley <joshua.rowley@nyu.edu>
2012-07-16 20:49:33 -0400

    Create relationship model and establish has_many belongs_to relationships with User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,6 +12,7 @@
 class User < ActiveRecord::Base
   attr_accessible :email, :name, :password, :password_confirmation
   has_many :microposts, dependent: :destroy
+  has_many :relationships, foreign_key: "follower_id", dependent: :destroy
 
   before_save { self.email.downcase! }
   before_save :create_remember_token
